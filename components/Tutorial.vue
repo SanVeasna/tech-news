bf4a3b723ccf40a8457f07fa8b7dfd5da698ed93
SanVeasna <sanveasna096@gmail.com>
2023-06-01 16:06:20 +0700

    update tutorial file

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,7 +1,9 @@
 <!-- Please remove this file from your project -->
 <template>
   <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
-    Welcome To My Page
+    <div style="font-size: 50px;">
+      Welcome To My Page
+    </div>
   </div>
 </template>
 
