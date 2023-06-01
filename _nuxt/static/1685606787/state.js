ef72776c6d750acd5cb4ae87567948057b80b015
SanVeasna <SanVeasna@users.noreply.github.com>
2023-06-01 08:33:42 +0000

    deploy: 5a5fcabfba0597f8756f6f4f15946e5919a9977e

--- _nuxt/static/1685606787/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftech-news\u002F_nuxt\u002Fstatic\u002F1685606787",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftech-news\u002F",assetsPath:"\u002Ftech-news\u002F_nuxt\u002F",cdnURL:a}}}}(null));
