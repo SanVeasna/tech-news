5f7e5d1fe1731b4f0b448ccb398e2910533afb73
SanVeasna <sanveasna096@gmail.com>
2023-06-01 16:14:10 +0700

    update tutorial file

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -3,6 +3,7 @@
   <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
     <div style="font-size: 50px;">
       Welcome To My Page
+      <div>ស្វាគមន៍មកកាន់គេហទំព័ររបស់ខ្ញុំបាទ</div>
     </div>
   </div>
 </template>
